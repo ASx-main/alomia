ec1cb438a8338494fd3c6d29b367fa47c3b707ef
NS <nsxmain@gmail.com>
2022-07-23 14:10:48 +0300

    Add rest api

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,5 +54,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  serverMiddleware: [
+    { path: '/api', handler: '@/serverMiddleware/rest.js' }
+  ]
 }
---
+++ serverMiddleware/rest.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser')
+const app = require('express')()
+
+app.use(bodyParser.json())
+
+app.all('/test', (req, res) => {
+  res.json({ data: 'Test' })
+})
+
+module.exports = app
